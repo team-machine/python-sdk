45d52348f6f8161ef8315f0c6a65aa6b5a5dda1b
Matti John <matti.john@gmail.com>
2019-10-23 14:56:20 +0100

    Optional activity date parameters

--- teammachine/query.py
+++ teammachine/query.py
@@ -3,8 +3,13 @@ class Query:
         self.node_type = node_type
         self._gql = gql
 
-    def activity(self, start_date, end_date, **kwargs):
-        query_params = {"start_date": start_date, "end_date": end_date, **kwargs}
+    def activity(self, start_date=None, end_date=None, **kwargs):
+        query_params = {
+            "start_date": self._date_param(start_date),
+            "end_date": self._date_param(end_date),
+            **kwargs,
+        }
+
         query_filter = ", ".join(
             '{name}:"{value}"'.format(name=name, value=value)
             for name, value in query_params.items()
@@ -14,6 +19,21 @@ class Query:
             query_filter = "(" + query_filter + ")"
 
         return self._activity_request(query_filter)
+
+    def _date_param(self, param):
+        if param is None:
+            return param
+
+        if isinstance(param, str):
+            return param
+
+        return param.isoformat()
+
+    def _format_param(self, name, value):
+        if isinstance(value, str):
+            value = "{}".format(value)
+
+        return "{name}:{value}".format(name=name, value=value)
 
     def _activity_request(self, query_filter):
         result = self._gql.request(
