e3db06ad645de165dd6742c34f1b60a4bc2e2e10
Matti John <matti.john@gmail.com>
2019-10-28 18:07:38 +0000

    Add Field type and indent query strings

--- teammachine/fields.py
+++ teammachine/fields.py
@@ -2,6 +2,7 @@ from collections import Mapping
 
 __all__ = [
     "Query",
+    "Field",
     "ObjectField",
     "EventField",
     "IdentityField",
@@ -39,41 +40,24 @@ def _format_arguments(args):
     return args
 
 
-def _format_field(name, field):
-    if name == field:
-        return name
-    else:
-        return name + str(field)
+def _format_field(name, field, indent=0):
+    return "  " * indent + name + field._to_string(indent=indent)
 
 
-def _format_fields(fields):
-    return "\n".join(_format_field(name, field) for name, field in fields.items())
-
-
-def _create_object_field(value):
-    args = []
-    kwargs = {}
-
-    for field in value:
-        if isinstance(field, (str, ObjectField)) or field is None:
-            args.append(field)
-        elif isinstance(field, Mapping):
-            kwargs.update(field)
-        else:
-            raise ValueError(
-                "Invalid field: %s. Must be str, dict or subclass of Field" % field
-            )
-    return ObjectField(*args, **kwargs)
+def _format_fields(fields, indent=0):
+    return "\n".join(
+        _format_field(name, field, indent) for name, field in fields.items()
+    )
 
 
 def _create_fields(obj):
     result = {}
 
     for name, value in obj.items():
-        if isinstance(value, (str, ObjectField)) or value is None:
+        if isinstance(value, str):
+            result[name] = Field()
+        elif isinstance(value, Field) or value is None:
             result[name] = value
-        elif isinstance(value, list):
-            result[name] = _create_object_field(value)
         else:
             raise ValueError(
                 "Invalid field: %s. Must be str, dict or subclass of Field" % value
@@ -82,18 +66,44 @@ def _create_fields(obj):
     return result
 
 
-class ObjectField:
+class Field:
+    def __init__(self):
+        self._args = {}
+        self._args_view = ArgsView(self)
+
+    def __str__(self):
+        return self._to_string()
+
+    def _to_string(self, indent=0):
+        return "{args}".format(args=_format_arguments(self._args))
+
+    def __call__(self, **kwargs):
+        instance = self._clone()
+        instance._args = {**instance._args, **kwargs}
+        return instance
+
+    def _clone(self, *args, **kwargs):
+        instance = self.__class__()
+        instance._args = self._args
+        return instance
+
+    @property
+    def arguments(self):
+        return self._args_view
+
+
+class ObjectField(Field):
     default_fields = {"tm_id": "tm_id", "tm_display_name": "tm_display_name"}
 
     def __init__(self, *fields, **kwargs):
         if not all(isinstance(field, str) for field in fields):
             raise ValueError("fields passed in as *args must be strings")
 
-        fields = {x: x for x in fields}
+        fields = _create_fields({x: x for x in fields})
         fields.update(_create_fields(kwargs))
 
         if not fields:
-            fields = {**self.default_fields}
+            fields = _create_fields(self.default_fields)
 
         self._fields = {k: v for k, v in fields.items() if v is not None}
         self._fields_view = FieldsView(self)
@@ -101,11 +111,13 @@ class ObjectField:
         self._args_view = ArgsView(self)
 
     def __str__(self):
-        return self._to_string(self._args, self._fields)
+        return self._to_string()
 
-    def _to_string(self, args, fields):
-        return "{args} {{\n{fields}\n}}".format(
-            args=_format_arguments(args), fields=_format_fields(fields)
+    def _to_string(self, indent=0):
+        return "{args}{{\n{fields}\n{indent}}}".format(
+            args=_format_arguments(self._args),
+            fields=_format_fields(self._fields, indent + 1),
+            indent="  " * indent,
         )
 
     def __call__(self, **kwargs):
@@ -117,10 +129,6 @@ class ObjectField:
         instance = self.__class__(*args, **{**self._fields, **kwargs})
         instance._args = self._args
         return instance
-
-    @property
-    def arguments(self):
-        return self._args_view
 
     @property
     def fields(self):
@@ -174,8 +182,10 @@ class Query(ObjectField):
     def __call__(self, *args, **kwargs):
         return self
 
-    def _to_string(self, args, fields):
-        return "query {{\n{fields}\n}}".format(fields=_format_fields(fields))
+    def _to_string(self, indent=0):
+        return "query {{\n{fields}\n}}".format(
+            fields=_format_fields(self._fields, indent + 1)
+        )
 
 
 class IdentityField(ObjectField):
